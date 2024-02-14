5befd7a51549a17c8c653050935bf7c6ee179aef
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-02-14 21:39:09 +0530

    Practicing ZOD

--- Week3/authentication.js
+++ Week3/authentication.js
@@ -39,6 +39,69 @@ app.use(function(err, req,res, next){
 }); 
  */
 
+
+/* 
+email: string => email
+password: atleast 8 letters
+country: "IN", "US"
+*/
+
+const schema1 = zod.object({
+    email: zod.string(),
+    password: zod.string(),
+    country: zod.literal("IN").or(zod.literal("US")),
+    kidneys: zod.array(zod.number())
+})
+
+
+// if this is an array of number with atleast 1 input, return true, else return false
+function validateInput(arr){
+    if(typeof arr == 'object' && arr.length >= 1 && arr[0] == "number" && arr.every((item) => typeof item == 'number')) {
+        return true
+    }
+}
+
+
+function validateInputWithZod(arr){
+    const schema = zod.array(zod.number());
+
+    const response = schema.safeParse(arr);
+    console.log(response); 
+}
+
+
+// only accept if the input in id is a email address and password should be atleast 8 character
+function idPassowrdZod(obj){{
+    const schema = zod.object({
+        email: zod.string().email(), 
+        password: zod.string().min(8)
+
+    })
+
+    const response = schema.safeParse(obj);
+    console.log(response); 
+
+}}
+
+idPassowrdZod({
+    email: "ashukumar007@gmail.com", 
+    password: "11111111"
+})
+
+
+app.post('/login', (req, res) =>{
+    const inputs = req.body;
+    const response = idPassowrdZod(inputs)
+        if(!response.success){
+            res.json({
+                msg: "Your input is not correct"
+            }) 
+            return;
+        }
+    }
+)
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`)
