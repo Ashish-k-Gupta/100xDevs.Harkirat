c71ceba782154509c34ba4f5e1fb81ed99943f0e
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-02-24 19:36:21 +0530

    closing week3 here

--- Week3/practice.js
+++ Week3/practice.js
@@ -1,19 +1,13 @@
 const express = require("express");
-const zod = require("zod");
 const jwt = require("jsonwebtoken");
 const app = express();
-app.use(express.json());
+const zod = require("zod");
+app.listen(express.json());
 
 
 const emailSchema = zod.string().email();
 const passwordSchema = zod.string().min(6);
 
-if(!emailSchema.success || !passwordSchema.success){
-    return null;
-}else{
-    
-    function signJwt(username, password){
-        const signature = jwt.sign({username}, "1234567")
-        return signature
-    }
-}
+
+
+function signJwt(username, )
