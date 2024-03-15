61030262f98acd97642e3e06213e7f0627b81970
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-03-15 11:10:35 +0530

    return & re-rendring

--- week6/Week-6.1-project/src/App.jsx
+++ week6/Week-6.1-project/src/App.jsx
@@ -1,44 +1,26 @@
-import React, { useState } from 'react'
+import React, { Component, useState } from 'react'
 import './App.css'
 
-function App() {
 
-  
-  
-  return ( 
-
-
-    <div>
-   
-    <Header title="Ashish2"></Header>
-    <Header title="Ashish3"></Header>
-    <Header title="Ashish4"></Header>
-    <Header title="Ashish5"></Header>
-    <Header title="Ashish6"></Header>
-    </div>
-
-
-    )
-}
-  
-function HeaderWithButton(){
-  const [title, setTitle] = useState("My name is Slim Shady")
-function randomNum(){
-  setTitle("My name is " + Math.random());
-}
-
-return 
-  <div>
- <button onClick={randomNum}>Re-Render</button>
-    <Header title={title}></Header>
-  </div>
-}
-
-
-function Header({title}){
+function App(){
   return <div>
-    {title}
+  <CardWrapper innerComponent = {<TextComponent />}/>
+  </div>
+
+}
+
+function CardWrapper({innerComponent}){
+  return <div style={{border: "2px Solid Red", padding: 20}}>
+  {innerComponent}
   </div>
 }
+
+
+function TextComponent(){
+  return <div>
+    Hi There!!
+  </div>
+}
+
 
 export default App
---
+++ week6/returnRerendring.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return ( 
+    <div>
+    <HeaderWithButton/>
+    <Header title="Ashish2"></Header>
+    <Header title="Ashish3"></Header>
+    <Header title="Ashish4"></Header>
+    <Header title="Ashish5"></Header>
+    <Header title="Ashish6"></Header>
+    </div>
+    )
+}
+  
+function HeaderWithButton(){
+  const [title, setTitle] = useState("My name is Slim Shady")
+function randomNum(){
+  setTitle("My name is " + Math.random());
+}
+
+return <div>
+ <button onClick={randomNum}>Re-Render</button>
+    <Header title={title}></Header>
+  </div>
+}
+
+
+const Header = React.memo(function Header({title}){
+  return <div>
+    {title}
+  </div>
+})
+
+export default App
