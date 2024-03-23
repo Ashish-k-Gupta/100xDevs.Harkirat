baeff5ae0556cf90067a44b61bf7c860470f1d97
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-03-23 09:07:55 +0530

    add number from 1 to n, where n is input by the user

--- week6/week6.2/class6.2/src/App.jsx
+++ week6/week6.2/class6.2/src/App.jsx
@@ -76,8 +76,15 @@ export default App
 ## useMemo
 It's a mildly DSA concept
 it means remembering some output given an input and not computing it again.
-*/
 
+
+// PROBLEM STATEMENT
+
+If i ask you to create an app that does two things - 
+    1- Increases a counter by 1
+    2- Lets user put a value in an input box (n) and you need to show sum from 1 to n
+*/
+/* 
 function App(){
 
   const [sum, setSum] = useState(0);
@@ -102,3 +109,30 @@ return (
 }
 
 export default App
+
+
+ */
+
+function App(){
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  let count = 0;
+  for(let i = 1; i <= inputValue; i++){
+    count = count + i;
+  }
+
+  return <div>
+    <input onChange={function(e){
+      setInputValue(e.target.value);
+    }} placeholder={"Find sum from 1 to n"}></input>
+    <br />
+    sum from 1 to {inputValue} is {count}
+    <br/>
+    <button onClick={() => {
+      setCounter(counter + 1);
+    }}>Counter({counter})</button>
+  </div>
+}
+
+export default App;
