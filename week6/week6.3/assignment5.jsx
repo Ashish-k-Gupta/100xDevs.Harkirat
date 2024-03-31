c39d2092193d26eafb2c2ac95287f8bee6ddbeea
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-03-31 20:24:56 +0530

    solved next assignment

---
+++ week6/week6.3/assignment5.jsx
@@ -0,0 +1,33 @@
+// You have been given a list of items you shopped from the grocery store
+// You need to calculate the total amount of money you spent
+import React, {useState, useMemo} from "react";
+
+function App() {
+  const [items, setItems] = useState([
+      {name: 'Chocolates', value: 10}, 
+      {name: 'Chips', value: 20}, 
+      {name: "Onion", value: 30},
+      {name: 'Tomato', value: 30},
+      // Add more items as needed
+  ]);
+
+
+  // Your code starts here
+  let totalValue = 0;
+  for(let i = 0; i < items.length; i++){
+    totalValue = totalValue + items[i].value;
+  }
+  // Your code ends here
+  return (
+    <div>
+      <ul>
+        {items.map((item, index) =>(
+          <li key={index}> {item.name} - Price: ${item.value}</li>
+        ))}
+      </ul>
+      <p>Total Value: {totalValue}</p>
+    </div>
+  );
+};
+
+export default App;
