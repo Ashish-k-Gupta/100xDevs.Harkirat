2f4766d29469d975dbd2c601db636f3fbc08fb71
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-04-08 14:03:24 +0530

    Lazy Loading, Suspense

--- Week7/vite-project/src/App.jsx
+++ Week7/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+/* import './App.css'
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import {Dashboard} from "./components/dashboard"
 import {Landing} from "./components/landing"
@@ -47,6 +47,56 @@ function App() {
     )
 
   }
+}
+
+export default App
+ */
+
+import React, { Suspense } from "react";
+import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
+const Landing = React.lazy( () =>  import("./components/landing"));
+const Dashboard = React.lazy( () => import("./components/dashboard"));
+
+function App(){
+  return (
+    <div>
+      <BrowserRouter>
+        <Appbar/>
+        <Routes>
+          <Route path="/dashboard" element = {<Suspense fallback={"Loading..."}> <Dashboard/></Suspense>} />
+          <Route path="/" element={<Suspense fallback={"Loading..."}> <Landing/> </Suspense>}/>
+        </Routes>
+      </BrowserRouter>
+      </div>
+
+  )
+
+  
+  function Appbar(){
+    const navigate = useNavigate();
+  
+      return (
+          <div style={{background: "Blue", color: "white"}}>
+  
+  
+      <button style={{padding: 5, margin: 5}} onClick={()=>{
+        window.location.reload()
+      }}>Refresh</button>
+  
+      <button style={{padding: 5, margin: 5}} onClick={()=>{
+        navigate("/dashboard");
+      }}>Dashboard</button>
+  
+      <button style={{padding: 5, margin: 5}} onClick={() =>{
+        navigate("/");  
+      }}>Landing</button>
+  
+  
+      </div>
+  
+      )
+  
+    }
 }
 
 export default App
